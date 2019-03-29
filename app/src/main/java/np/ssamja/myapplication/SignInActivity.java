062370e3573b07660fc7130ddb7895f487e0fa27
iudragon <iudragon32@gmail.com>
2019-03-29 10:33:03 +0530

    15Order

--- app/src/main/java/np/ssamja/myapplication/SignInActivity.java
+++ app/src/main/java/np/ssamja/myapplication/SignInActivity.java
@@ -1,6 +1,7 @@
 package np.ssamja.myapplication;
 
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,17 +9,51 @@ import android.widget.Button;
 
 public class SignInActivity extends AppCompatActivity {
 
+    Button customerButton, driverButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        customerButton = findViewById(R.id.button_customer);
+        driverButton = findViewById(R.id.button_driver);
+
+        customerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                customerButton.setBackgroundColor(getResources().getColor(R.color.colorAccent));
+                customerButton.setTextColor(getResources().getColor(android.R.color.white));
+
+                driverButton.setBackgroundColor(getResources().getColor(android.R.color.white));
+                driverButton.setTextColor(getResources().getColor(R.color.colorAccent));
+            }
+        });
+
+        driverButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                driverButton.setBackgroundColor(getResources().getColor(R.color.colorAccent));
+                driverButton.setTextColor(getResources().getColor(android.R.color.white));
+
+                customerButton.setBackgroundColor(getResources().getColor(android.R.color.white));
+                customerButton.setTextColor(getResources().getColor(R.color.colorAccent));
+            }
+        });
+
         Button buttonLogin = findViewById(R.id.button_login);
         buttonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), CustomerMainActivity.class);
-                startActivity(intent);
 
+                ColorDrawable customerButtonColor = (ColorDrawable) customerButton.getBackground();
+                if (customerButtonColor.getColor() == getResources().getColor(R.color.colorAccent)) {
+                    Intent intent = new Intent(getApplicationContext(), CustomerMainActivity.class);
+                    startActivity(intent);
+                } else {
+                    Intent intent = new Intent(getApplicationContext(), DriverMainActivity.class);
+                    startActivity(intent);
+                }
             }
         });
     }
