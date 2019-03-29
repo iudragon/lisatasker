db07a0bcc18bcb1e57e2833ccffba1e6b63459f3
iudragon <iudragon32@gmail.com>
2019-03-29 09:16:01 +0530

    Initial commit

---
+++ app/src/main/java/np/ssamja/myapplication/SignInActivity.java
@@ -0,0 +1,25 @@
+package np.ssamja.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+        Button buttonLogin = findViewById(R.id.button_login);
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), CustomerMainActivity.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+}
